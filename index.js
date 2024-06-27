8a02b3fbea4ca2bbaecd19f3f9b397257b7b86e1
Thalyson Lima da Silva <devthalyson72@gmail.com>
2024-06-27 15:35:50 -0300

    index.js
    Primeiro fluxo de código ok!

--- index.js
+++ index.js
@@ -23,14 +23,19 @@ function inLista(n, l){
 
 function adicionar(){
     if(isNumber(num.value) & !inLista(num.value, valores)){
-        alert``
+        valores.push(Number(num.value))
+        item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
-        alert``
-    }   
+        alert`[ERRO]! Número inválido ou já existente!`
+    }
+    num.value = ``
+    num.focus()
 }
 
 
 
 //Sabemos que para buscar um valor dentro de um array utilizamos o indexOf() e dentro dos paranteses o elemento no qual queremos procurar
 //Feito isso o Js vai me retornar a posição de onde vem esse valor
-//
+//Vai ser feito uma procura de indexOf da posição numeros na lista l.
